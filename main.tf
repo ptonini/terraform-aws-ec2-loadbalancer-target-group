470c314e14403062c4fd5fa7d3df0abc63003ac5
ptonini <pedro.tonini@hotmail.com>
2023-10-05 14:54:34 -0300

    feat: added aws_autoscaling_attachment resource

--- main.tf
+++ main.tf
@@ -4,22 +4,27 @@ resource "aws_lb_target_group" "this" {
   vpc_id      = var.vpc.id
   target_type = var.target_type
   health_check {
-    enabled             = try(var.health_check["enabled"], false)
-    path                = try(var.health_check["path"], null)
-    matcher             = try(var.health_check["matcher"], null)
-    protocol            = try(var.health_check["protocol"], var.protocol)
-    port                = try(var.health_check["port"], null)
-    healthy_threshold   = try(var.health_check["healthy_threshold"], null)
-    interval            = try(var.health_check["interval"], null)
-    timeout             = try(var.health_check["timeout"], null)
-    unhealthy_threshold = try(var.health_check["unhealthy_threshold"], null)
+    enabled             = var.health_check.enabled
+    path                = var.health_check.path
+    matcher             = var.health_check.matcher
+    protocol            = var.health_check.protocol
+    port                = var.health_check.port
+    healthy_threshold   = var.health_check.healthy_threshold
+    interval            = var.health_check.interval
+    timeout             = var.health_check.timeout
+    unhealthy_threshold = var.health_check.unhealthy_threshold
   }
 }
 
 resource "aws_lb_target_group_attachment" "this" {
-  for_each          = toset(var.target_ids)
+  for_each          = var.target_ids
   target_group_arn  = aws_lb_target_group.this.arn
   target_id         = each.value
   availability_zone = var.target_type == "ip" ? "all" : null
 }
 
+resource "aws_autoscaling_attachment" "this" {
+  for_each               = var.autoscaling_groups
+  autoscaling_group_name = each.value
+  lb_target_group_arn    = aws_lb_target_group.this.arn
+}
--- variables.tf
+++ variables.tf
@@ -13,11 +13,28 @@ variable "vpc" {
 }
 
 variable "health_check" {
+  type = object({
+    enabled             = optional(bool, false)
+    path                = optional(string)
+    matcher             = optional(string)
+    protocol            = optional(string, "HTTP")
+    port                = optional(number)
+    healthy_threshold   = optional(string)
+    interval            = optional(string)
+    timeout             = optional(string)
+    unhealthy_threshold = optional(string)
+  })
   default = {}
 }
 
 variable "target_type" {}
 
 variable "target_ids" {
+  type    = set(string)
+  default = []
+}
+
+variable "autoscaling_groups" {
+  type    = set(string)
   default = []
 }
