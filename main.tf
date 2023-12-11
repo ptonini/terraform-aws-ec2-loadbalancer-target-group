144414714718d9c57ae741071299a6a6b830ae13
ptonini <pedro.tonini@hotmail.com>
2023-12-11 13:11:44 -0300

    fix: minor refactor

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_lb_target_group" "this" {
-  name = var.name
+  name        = var.name
   port        = var.port
   protocol    = var.protocol
   vpc_id      = var.vpc.id
   target_type = var.target_type
+
   health_check {
     enabled             = var.health_check.enabled
     path                = var.health_check.path
